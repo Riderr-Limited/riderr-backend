02921d8e69aa0332155a9f8961d01d27ee5275ab
Auwalu Muhammad Izziddin <auwalmuhammad5559@gmail.com>
2026-01-25 21:18:23 +0100

    Add new allowed origins for CORS

--- app.js
+++ app.js
@@ -21,7 +21,10 @@ const corsOptions = {
       'http://localhost:3000',
       'http://localhost:3001',
       'http://127.0.0.1:3000',
-      'http://127.0.0.1:3001'
+      'http://127.0.0.1:3001',
+      'https://riderr.ng',
+      'https://www.riderr.ng',
+      'https://riderrr.vercel.app'
     ];
     
     if (allowedOrigins.includes(origin)) {
